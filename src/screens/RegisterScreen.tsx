29c1ac91cd4b2aa439a6c79df192e7a8fe57e2f2
Juan Flores <elgalleto12393@gmail.com>
2025-09-05 11:33:06 -0600

    chore: remove unnecessary comment for cleaner code

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -477,7 +477,6 @@ const styles = StyleSheet.create({
   inputWrapper: {
     marginBottom: 20,
     borderRadius: 16,
-    // Inner neumorphic effect
     shadowColor: '#000',
     shadowOffset: {
       width: 4,
