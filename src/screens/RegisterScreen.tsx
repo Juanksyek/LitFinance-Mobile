23c0d3d2fcfae4b0922606672be4fb71b21f657b
Juan Flores <elgalleto12393@gmail.com>
2025-09-23 22:46:50 -0600

    chore: add import statements to RegisterScreen for React and dependencies

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,3 +1,4 @@
+// RegisterScreen.tsx
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Platform, ScrollView, Image, Modal } from 'react-native';
 import axios from 'axios';
