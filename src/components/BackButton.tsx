511e2a41ac6a6931c324b94ce09c64791ea10417
Juan Flores <elgalleto12393@gmail.com>
2025-10-08 22:48:00 -0600

    refactor: update Ionicons import to use @expo/vector-icons in BackButton component

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
 
 const BackButton = () => {
