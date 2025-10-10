f5190e73ef63fc7e90c540dea1bd916d671a3ba1
Juan Flores <elgalleto12393@gmail.com>
2025-10-09 23:05:32 -0600

    refactor: update Ionicons import to use @expo/vector-icons

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = {
   visible: boolean;
